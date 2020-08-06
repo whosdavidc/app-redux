7d2fee3a2f7827d0daa8b17eebe35e4d4adad7e8
fisurx <davinchocastro@gmail.com>
2020-08-06 09:11:39 -0300

    api called

--- src/components/Pokemones.jsx
+++ src/components/Pokemones.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import {useDispatch, useSelector} from "react-redux";
+import { GET_POKEMONS } from "../redux/pokeDucks";
+
 
 const Pokemones = () => {
+    const dispatch = useDispatch()
+
+    const pokemones = useSelector(store => store.pokemons.array) 
+
     return (
         <div>
             <h1>Lista de pokemons</h1>
+            <button onClick={() => dispatch(GET_POKEMONS())}>Click here</button>
+
+            {
+                pokemones.map(item => (
+                    <li key={item.name}>{item.name}</li>
+                ))
+            }
         </div>
     )
 }
--- src/redux/pokeDucks.js
+++ src/redux/pokeDucks.js
@@ -7,12 +7,12 @@ const datainicial = {
 }
 
 //types
-const GET_POKEMONS_SUCCESSFUL = "GET_POKEMOS_SUCCESFULL"
+const GET_POKEMONS_SUCCESSFULL = "GET_POKEMONS_SUCCESSFULL"
 
 //reducer
 export default function pokeReducer(state = datainicial , action){
     switch(action.type){
-        case GET_POKEMONS_SUCESSFUL:
+        case GET_POKEMONS_SUCCESSFULL:
             return {...state, array: action.payload}  //with {...state} were saying that the state, becomes from the data saved
         default:
             return state
@@ -20,14 +20,15 @@ export default function pokeReducer(state = datainicial , action){
 }
 
 //actions
-export const GET_POKEMONS_SUCESSFUL = () => async (dispatch, getState) =>{ //with the dispatch we can activate the reducer, and w/getState we get the initial data 
+export const GET_POKEMONS = () => async (dispatch, getState) =>{ //with the dispatch we can activate the reducer, and w/getState we get the initial data 
     try {
-        const res = await axios.get("https://pokemon?limit=100&offset=200")
+        const res = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=100&offset=200")
         dispatch({   
-            type: GET_POKEMONS_SUCCESSFUL,
-            payload: res.date.results
+            type: GET_POKEMONS_SUCCESSFULL,
+            payload: res.data.results
         })
     }catch(error){
         console.log(error);
+        console.log("aca rey")
     }
 }
